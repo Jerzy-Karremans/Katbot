6de052db56f510cf40476bc29638894634a4ee82
jerzy <jerzykarremans@hotmail.com>
2023-06-07 20:20:37 +0200

    yep

--- test-api-calls/WisperAI.py
+++
@@ -1,7 +0,0 @@
-import os
-import openai
-
-openai.api_key = os.getenv("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")
-
-audio_file= open("C:/Users/jerzy/source/repos/Katbot/test-api-calls/test.mp3", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
