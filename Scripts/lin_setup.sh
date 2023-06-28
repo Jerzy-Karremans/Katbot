1336e80c541bdec6bda07283f7d53e94e2ff3014
jerzy <jerzykarremans@hotmail.com>
2023-06-28 22:25:39 +0200

    update linux Scripts

--- Scripts/lin_setup.sh
+++ Scripts/lin_setup.sh
@@ -2,30 +2,30 @@
 
 # Check if Python is installed
 python3 --version >/dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "Python 3 is required but not found. Please install Python 3."
     exit 1
 fi
 
 # Check if pip is installed
 pip --version >/dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "pip is required but not found. Please install pip."
     exit 1
 fi
 
 # Check if the Katbot directory exists
-if [ ! -d "../../Katbot/" ]; then
+if [ ! -d "../..//Katbot" ]; then
     # Clone the GitHub repository
     git clone https://github.com/Jerzy-Karremans/Katbot
     cd Katbot
 else
     # Change directory to the cloned repository
-    cd ../../Katbot/
+    cd ../..
 fi
 
 # Change directory to the cloned repository
-mkdir -p config
+mkdir config
 
 # Create a virtual environment
 python3 -m venv env
@@ -34,15 +34,17 @@ python3 -m venv env
 source env/bin/activate
 
 # Upgrade pip
-python -m pip install --upgrade pip
+pip install --upgrade pip
 
 # Install the requirements and dependencies
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Create the api_keys.json file
-echo "{" > config/api_keys.json
-echo '   "openApi": "your_api_key_1"' >> config/api_keys.json
-echo "}" >> config/api_keys.json
+echo '{' > config/api_keys.json
+echo '   "openApi": "your_open_api_key",' >> config/api_keys.json
+echo '   "elevenlabs": "your_eleven_api_key",' >> config/api_keys.json
+echo '   "usingEleven": false' >> config/api_keys.json
+echo '}' >> config/api_keys.json
 echo "Created a default api_keys.json file. Please update the API keys in 'config/api_keys.json' with your actual keys."
 
 # Deactivate the virtual environment
